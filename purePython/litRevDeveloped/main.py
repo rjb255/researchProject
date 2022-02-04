9f66f5478ea055f7bdd6e95f74111fd53d1b1d1e
rjb255 <rjb255@cam.ac.uk>
2022-02-04 12:07:30 +0000

    added

---
+++ purePython/litRevDeveloped/main.py
@@ -0,0 +1,128 @@
+import sys
+import os
+from pprint import pprint
+from typing import List
+from functools import partial
+from multiprocessing import Process, Queue
+
+
+import pandas as pd
+import numpy as np
+from scipy.spatial import distance_matrix as dist_mat
+from sklearn.linear_model import BayesianRidge as BR
+
+proj_path = os.path.join(
+    "/", "home", "rjb255", "University", "ChemEng", "ResearchProject"
+)
+sys.path.insert(1, proj_path)
+
+from purePython.modules.shared.custom import split, getPI, validate, Models
+
+
+def loopDecorator(iterations, size):
+    def decorator(func):
+        def inner(model, X_train, Y_train, X_unknown, Y_unknown, X_test, test, set_num):
+            def doubleInner(f, q):
+                X, Y = pd.DataFrame(X_train), pd.Series(Y_train)
+                x, y = pd.DataFrame(X_unknown), pd.Series(Y_unknown)
+
+                score_record = []
+                for i in range(iterations):
+                    next_index, score = f(model, X, Y, x, y, X_test, test)
+
+                    X, Y, x, y = getPI((X, Y), (x, y), next_index[:size])
+                    score_record.append(score)
+                q.put(score_record)
+
+            q = [Queue(), Queue()]
+            active_learning = Process(target=doubleInner, args=(func, q[0]))
+            base_run = Process(target=doubleInner, args=(base, q[1]))
+            active_learning.start(), base_run.start()
+            final_score_AL = q[0].get()
+            final_score_base = q[1].get()
+            active_learning.join(), base_run.join()
+
+            _file = os.path.join(
+                proj_path,
+                "purePython",
+                "litRevDeveloped",
+                str(type(model).__name__),
+                str(func.__name__),
+                str(iterations),
+                str(size),
+                str(len(X_train)),
+                f"{set_num}.csv",
+            )
+
+            os.makedirs(os.path.dirname(_file), exist_ok=True)
+
+            with open(_file, "w") as f:
+                print("\nwriting")
+                f.write(f"{final_score_AL}\n{final_score_base}")
+
+        return inner
+
+    return decorator
+
+
+def base(model, X_train, Y_train, X_unknown, Y_unknown, X_test, test):
+    model.fit(X_train, Y_train)
+    Y_predict = model.predict(X_unknown)
+    next_index = X_unknown.index
+    score = test(model.predict(X_test))
+    return next_index, score
+
+
+@loopDecorator(15, 160)
+def uncertainty_sampling(model, X_train, Y_train, X_unknown, Y_unknown, X_test, test):
+    model.fit(X_train, Y_train)
+    Y_predict, Y_error = model.predict(X_unknown, return_std=True)
+    next_index = X_unknown.index[np.argsort(-Y_error)]
+    score = test(model.predict(X_test))
+    return next_index, score
+
+
+def broadBase(model, X_train, Y_train, X_unknown, Y_unknown, X_test, test, set_num):
+    density(X_train, X_unknown)
+
+
+def density(x1, x2):
+    tree = np.array(dist_mat(x1, x2))
+    tree[tree == 0] = np.min(tree[np.nonzero(tree)])
+    print(1)
+
+
+def main(*, set_num=0, model, sampling_method):
+    paths = ["data_CHEMBL313.csv", "data_CHEMBL2637.csv", "data_CHEMBL4124.csv"]
+    data_sets: List[pd.DataFrame] = [
+        pd.read_csv(
+            os.path.join(proj_path, "data", "chembl", "Additional_datasets", path)
+        )
+        for path in paths
+    ]
+    data: List[pd.DataFrame] = data_sets[set_num].sample(frac=1, random_state=1)
+    X_known, Y_known, X_unknown, Y_unknown, X_test, Y_test = split(data, 160)
+    test = partial(validate, Y_test)
+    models = {"BayesianRidge": BR()}
+
+    defaults = (
+        models[model],
+        X_known,
+        Y_known,
+        X_unknown,
+        Y_unknown,
+        X_test,
+        test,
+        set_num,
+    )
+    sampling_methods = {
+        "uncertainty_sampling": lambda: uncertainty_sampling(*defaults),
+        "broadBase": lambda: broadBase(*defaults),
+    }
+    sampling_methods[sampling_method]()
+
+
+if __name__ == "__main__":
+    data_set_num = 0
+
+    main(model="BayesianRidge", sampling_method="broadBase")
--- purePython/modules/shared/custom.py
+++ purePython/modules/shared/custom.py
@@ -1,17 +1,21 @@
+import numpy as np
 import pandas as pd
 from pandas.core.frame import DataFrame
+from sklearn.metrics import mean_squared_error as mse
 
-def split(data: DataFrame, n: int=20):
+
+def split(data: DataFrame, n: int = 20):
     ## Splits data (dataframe) insto a known section, an unknown section, and a testing section
-    
-    splitBoundary = [n, int(0.9*len(data))]
-    X1 = data.iloc[:splitBoundary[0], 2:]
-    Y1 = data.iloc[:splitBoundary[0], 1]
-    X2 = data.iloc[splitBoundary[0]:splitBoundary[1], 2:]
-    Y2 = data.iloc[splitBoundary[0]:splitBoundary[1], 1]
-    X3 = data.iloc[splitBoundary[1]:, 2:]
-    Y3 = data.iloc[splitBoundary[1]:, 1]
+
+    splitBoundary = [n, int(0.9 * len(data))]
+    X1 = data.iloc[: splitBoundary[0], 2:]
+    Y1 = data.iloc[: splitBoundary[0], 1]
+    X2 = data.iloc[splitBoundary[0] : splitBoundary[1], 2:]
+    Y2 = data.iloc[splitBoundary[0] : splitBoundary[1], 1]
+    X3 = data.iloc[splitBoundary[1] :, 2:]
+    Y3 = data.iloc[splitBoundary[1] :, 1]
     return X1, Y1, X2, Y2, X3, Y3
+
 
 def getPI(known: tuple, unknown: tuple, index: int):
     X_known, Y_known = known
@@ -23,6 +27,61 @@ def getPI(known: tuple, unknown: tuple, index: int):
 
     return X_known, Y_known, X_unknown, Y_unknown
 
-def bubbles(index: tuple, indicies: list, data: DataFrame):
-    index1, index2 = index
-    
+
+class Models:
+    """Creating a new model as an amalgamation of other models"""
+
+    def __init__(self, m: list):
+        """Defining the underlying models used
+
+        Args:
+            m (list): A list of models to be used in concert with each other
+        """
+        self.models = m
+
+    def fit(self, X, Y, *args):
+        """Fitting the models collectively as seen in most models.
+
+        Args:
+            X (DataFrame): data points to be fitted
+            Y (DataFrame): labels of fitted data
+        """
+        self.trainedModels = [m.fit(X, Y, *args) for m in self.models]
+
+    def predict(self, X, *args):
+        """Equivelent to predict seen in other models.
+
+        Args:
+            X (DataFrame): the data points to be predicted
+
+        Returns:
+            ndArray: the mean of the prediction from the other models.
+        """
+        return np.mean([m.predict(X, *args) for m in self.trainedModels], axis=0)
+
+    def predict_error(self, X, *args):
+        """Similar to predict but also returns the standard deviation of the models predictions
+
+        Args:
+            X (DataFrame): The data points to be predicted.
+
+        Returns:
+            (ndArray, ndArray): The mean and standard deviation of the predictions from each model.
+        """
+        predictions = [m.predict(X, *args) for m in self.trainedModels]
+        mean = np.mean(predictions, axis=0)
+        std = np.std(predictions, axis=0, ddof=1)
+        return mean, std
+
+
+def validate(y_true, y_false):
+    """Validation of the model.
+
+    Args:
+        y_true (DataFrame): The true test labels
+        y_false (ndArray): The predicted test values
+
+    Returns:
+        float: score for the model prediction
+    """
+    return mse(y_true, y_false, sample_weight=y_true)
