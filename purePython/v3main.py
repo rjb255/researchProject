6600d94d5a68ff29c95778cc8a352f913ee79064
rjb255 <rjb255@cam.ac.uk>
2022-03-02 14:29:04 +0000

    too much data

--- purePython/v3main.py
+++ purePython/v3main.py
@@ -31,9 +31,16 @@ from purePython.modules.shared.custom import split, getPI, Models
 # endregion
 
 
+def spec_max(x):
+    if len(x) > 0:
+        return max(x)
+    else:
+        return x
+
+
 def score(Y_test, kwargs, q):
     y_predict = kwargs["model"].predict(kwargs["X_test"])
-    q.put(mse(y_predict, Y_test))
+    q.put(mse(y_predict, Y_test, sample_weight=Y_test))
 
 
 def framework(
@@ -59,7 +66,9 @@ def framework(
         processes = []
         for i in range(iterations):
             if len(x) > 0:
-                next_index = f(m, X, Y, x, mem)
+                m.fit(X, Y)
+                ranking = f(m, X, Y, x, mem)
+                next_index = x.index[np.argsort(ranking)]
                 X, Y, x, y = getPI((X, Y), (x, y), next_index[:sample_size])
             score_record.append(Queue())
             processes.append(
@@ -87,41 +96,52 @@ def framework(
 
     os.makedirs(os.path.dirname(_file), exist_ok=True)
 
-    with open(_file, "w") as f:
-        print(f"\nwriting {_file}")
-        f.write(str(results))
+    print(f"\nwriting {_file}")
+    pd_results = pd.DataFrame(
+        results,
+        columns=list(
+            range(len(X_train), len(X_train) + iterations * sample_size, sample_size)
+        ),
+        index=[a.__name__ for a in algorithm],
+    )
+    pd_results.to_csv(_file)
 
 
 def base(m, X, Y, x, *args, **kwargs):
-    m.fit(X, Y)
     next_index = x.index
     return next_index
 
 
 def uncertainty_sampling(m, X, Y, x, *args, **kwargs):
-    m.fit(X, Y)
     _, Y_error = m.predict(x, return_std=True)
-    next_index = x.index[np.argsort(-Y_error)]
-    return next_index
+    return -Y_error
+
+
+def region_of_disagreement(m, X, Y, x, *args, **kwargs):
+    _, Y_error = m.predict_error(x)
+    return -Y_error
 
 
 def broad_base(m, X, Y, x, mem, *args, **kwargs):
     rho = density(X, x, mem)
-    m.fit(X, Y)
-    next_index = x.index[np.argsort(rho)]
-    return next_index
+    return rho
 
 
-def rod_hotspots(m, X, Y, *args, **kwargs):
-    r = 5
-    m.fit(X, Y)
-    pred, err = m.predict_error(x)
-    tree = KDTree(X.values)
-    indicies = tree.query_ball_point(np.array(x), r, workers=-1)
-    sdev = [err[i] for i in indicies]
+def rod_hotspots(m, X, Y, x, *args, **kwargs):
+    Y, Y_error = m.predict_error(x)
+    err = -Y_error
+    series = np.ones_like(err)
+    tree = KDTree(x.values)
+    for r in range(2, 9):
+        indicies = tree.query_ball_point(np.array(x), r, workers=-1)
+        sdev = [err[i] for i in indicies]
+        sdevMax = [spec_max(s) for s in sdev]
+        series += sdevMax == err
+        print(f"{r}", end="\r")
+    return series * err * Y
 
 
-def hotspots():
+def clusterise():
     pass
 
 
@@ -142,28 +162,33 @@ def main():
         for path in paths
     ]
 
-    set_num = 2
+    set_num = 0
     data: List[pd.DataFrame] = data_sets[set_num].sample(frac=1, random_state=1)
 
-    X_known, Y_known, X_unknown, Y_unknown, _, _ = split(data, 1, frac=1)
+    X_known, Y_known, X_unknown, Y_unknown, _, _ = split(data, 5, frac=1)
     X_test, Y_test = pd.concat([X_known, X_unknown]), pd.concat([Y_known, Y_unknown])
     models = {
         "BayesianRidge": BR(),
-        "KKK": KNN(n_jobs=-1),
+        "KNN": KNN(n_jobs=-1),
         "RandomForrest": RFR(random_state=1),
     }
     algorithms = {
         "dumb": base,
         "uncertainty_sampling": uncertainty_sampling,
+        "rod": region_of_disagreement,
         "broad": broad_base,
+        "mine": rod_hotspots,
     }
 
     # For when this isn't the only one: makes keeping track easier
-    model = Models([models["BayesianRidge"]])
+    model = Models([models["BayesianRidge"], models["KNN"], models["RandomForrest"]])
+
     algorithm = (
-        algorithms["uncertainty_sampling"],
+        algorithms["mine"],
+        algorithms["rod"],
         algorithms["dumb"],
     )
+
     framework(
         X_known,
         Y_known,
@@ -173,8 +198,8 @@ def main():
         Y_test,
         model,
         algorithm,
-        iterations=10,
-        sample_size=160,
+        iterations=20,
+        sample_size=100,
         score=score,
     )
 
