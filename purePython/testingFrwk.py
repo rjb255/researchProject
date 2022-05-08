7a9aaede638d3d1650386582c263361252ddd8bd
rjb255 <rjb255@cam.ac.uk>
2022-05-08 22:33:40 +0100

    maybe?

--- purePython/testingFrwk.py
+++ purePython/testingFrwk.py
@@ -49,7 +49,7 @@ def to_minimise(data_set, alpha, alg, ongoing=[]):
     with Pool() as p:
         scores = p.map(temp, [data for data in data_set], chunksize=1)
     scores = np.array(scores)
-    ongoing.append(([alpha] + list(scores)))
+    ongoing.append(([alpha] + [score[-1] for score in scores]))
     # print(len(scores[:, -1]))
     print(f"alpha score: {np.mean(scores[:,-1])}")
     return np.mean(scores[:, -1])
